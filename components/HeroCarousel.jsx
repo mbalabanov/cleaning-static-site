1e112d919b2f9431a1358bf67f306e18e9113b06
Marin Balabanov <marincomics@gmail.com>
2022-10-08 23:02:47 +0200

    Removes separate file for data, removes redundant image

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -1,7 +1,67 @@
-import heroImageData from '../data/hero-image-data'
-
 export default function HeroCarousel() { 
-    console.log(heroImageData)
+    const heroImageData = [
+        {
+            imagePath: "/photos/carousel/cleaning-counter.jpg",
+            imageCaption: "Apartment Cleaning",
+            imageDescription: "We clean your AirBnB property, short-term rental or Booking.com apartment. Have your apartment or house cleaned regularly by our specialist staff."
+        },
+        {
+            imagePath: "/photos/carousel/facade.jpg",
+            imageCaption: "Exterior Cleaning",
+            imageDescription: "We ensure that your building makes a dazzling impression from the outside! We clean your facade and outdoor facilities such as parking lots, sidewalks and courtyards."
+        },
+        {
+            imagePath: "/photos/carousel/office-cleaning.jpg",
+            imageCaption: "Office Cleaning",
+            imageDescription: "We turn your offices into a spotless and  well-kept workplace. We clean all types of office space - from medium-sized companies to office floors of large companies, as well as administrative and school offices."
+        },
+        {
+            imagePath: "/photos/carousel/restaurant.jpg",
+            imageCaption: "Catering Cleaning",
+            imageDescription: "As a restaurateur, you not only care about the well-being of your guests, you also pay attention to hygiene in your kitchen, seating area and food stores. We help you ensure hygene and safety."
+        },
+        {
+            imagePath: "/photos/carousel/construction-site.jpg",
+            imageCaption: "Construction Cleaning",
+            imageDescription: "As a flexible service provider, we are able to quickly bring order to the construction site. Our service also includes fine construction cleaning, general construction cleaning, the protection of built-in parts and the establishment of construction safety."
+        },
+        {
+            imagePath: "/photos/carousel/cleaning-windows.jpg",
+            imageCaption: "Window Cleaning",
+            imageDescription: "We clean your windows with and without cleaning their frames and also make all other glass surfaces shine with care and caution."
+        },
+        {
+            imagePath: "/photos/carousel/basic-cleaning.jpg",
+            imageCaption: "Basic Cleaning",
+            imageDescription: "Sometimes a space has not been seen to in a while - call us for a thoroughly cleaning. If you are interested in the basic cleaning of your office or apartment, then we are your ideal partner."
+        },
+        {
+            imagePath: "/photos/carousel/home-care.jpg",
+            imageCaption: "Home Care",
+            imageDescription: "We look after your property or your residential building reliably and professionally. So you can feel carefree, comfortable and safe."
+        },
+        {
+            imagePath: "/photos/carousel/hotel-room.jpg",
+            imageCaption: "Hotel Cleaning and House Keeping",
+            imageDescription: "So that the first impression is right: We handle house keeping in your hotel rooms and take care of cleanliness and hygene in every corner of your hotel."
+        },
+        {
+            imagePath: "/photos/carousel/doctors-practice.jpg",
+            imageCaption: "Cleaning in your Doctor's Practice",
+            imageDescription: "Cleanliness alone is not enough in your practice, you want thorough hygiene and reliable disinfection in your office, waiting room and bathrooms."
+        },
+        {
+            imagePath: "/photos/carousel/stairs.jpg",
+            imageCaption: "Staircase Cleaning",
+            imageDescription: "We ensure professional stairwell cleaning in Vienna. Where many people work or come as visitors, entrance areas and stairwells have to withstand extreme loads simply because of the frequency of use."
+        },
+        {
+            imagePath: "/photos/carousel/winter-service.jpg",
+            imageCaption: "Winter and Snow Service",
+            imageDescription: "We reliably clear snow and ice. We will be there for you in this winter."
+        }
+    ]
+    
     return(
         <h2 className="text-center">Hero Carousel goes here.</h2>
     )
