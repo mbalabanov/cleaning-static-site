d08cee563f06f63d7e4e02243045c06c1a0c16af
Marin Balabanov <marincomics@gmail.com>
2022-10-08 22:59:55 +0200

    Adds data for carousel, corrects names of images

--- components/HeroCarousel.jsx
+++ components/HeroCarousel.jsx
@@ -1,4 +1,7 @@
+import heroImageData from '../data/hero-image-data'
+
 export default function HeroCarousel() { 
+    console.log(heroImageData)
     return(
         <h2 className="text-center">Hero Carousel goes here.</h2>
     )
